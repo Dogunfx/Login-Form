8bbc10a026165088632da6a1ed399075acdda8ae
dogunfx <dogunfx@gmail.com>
2024-06-19 16:56:29 +0100

    update mobile design

--- app/components/login.jsx
+++ app/components/login.jsx
@@ -3,7 +3,7 @@ import { Button, Input, Form, Card } from "antd";
 
 export default function Login() {
   return (
-    <Card title="My Login Form" className="w-1/2 m-auto mt-52">
+    <Card title="My Login Form" className="sm:w-1/2 m-auto mt-52">
       <p>Please Enter your details below</p>
       <Form>
         <Form.Item>
