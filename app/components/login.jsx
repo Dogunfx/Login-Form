035979878724b3ecb1112e56c0d0f5aa3335598e
dogunfx <dogunfx@gmail.com>
2024-06-25 16:36:21 +0100

    added login

--- app/components/login.jsx
+++ app/components/login.jsx
@@ -1,34 +1,45 @@
 "use client";
 import { Button, Input, Form, Card, message, Typography } from "antd";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
   var [email, setEmail] = useState("");
+  var [password, setPassword] = useState("");
+  var router = useRouter();
 
-  function handleButtonClick() {
-    message.info(email);
+  function handleLogin() {
+    if (email == "dogunfx@gmail.com" && password == "12345") {
+      message.success("Login Successful");
+      router.push("/dashboard");
+    } else {
+      message.error("Login Fail");
+    }
   }
 
-  function handleChange(evt) {
-    email = evt.target.value;
-    setEmail(email);
+  function handleEmailInput(evt) {
+    setEmail(evt.target.value);
+  }
+  function handlePasswordInput(evt) {
+    setPassword(evt.target.value);
   }
 
   return (
     <Card title="My Login Form" className="max-w-md mx-auto my-44">
-      <Typography.Title className="text-green-900">
-        Hello world
-      </Typography.Title>
+      <Typography.Title className="text-green-900">Login Form</Typography.Title>
       <p>Please Enter your details below</p>
       <Form>
         <Form.Item>
-          <Input placeholder="Enter Email" onChange={handleChange} />
+          <Input placeholder="Enter Email" onChange={handleEmailInput} />
         </Form.Item>
         <Form.Item>
-          <Input.Password placeholder="Enter Password" />
+          <Input.Password
+            placeholder="Enter Password"
+            onChange={handlePasswordInput}
+          />
         </Form.Item>
       </Form>
-      <Button type="primary" onClick={handleButtonClick}>
+      <Button type="primary" onClick={handleLogin}>
         Login
       </Button>
     </Card>
---
+++ app/dashboard/page.jsx
@@ -0,0 +1,10 @@
+"use client";
+import { Card, Typography } from "antd";
+
+export default function Dashboard() {
+  return (
+    <Card title="Timer Dashboard">
+      <Typography.Title>My Dashboard</Typography.Title>
+    </Card>
+  );
+}
